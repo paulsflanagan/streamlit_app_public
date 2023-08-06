e76c61129b35b22de1ddc77200566595eff316e5
Paul S Flanagan <paulsflanagan@gmail.com>
2023-08-06 21:52:19 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,14 +21,13 @@ with st.sidebar:
 import wget
 
 
-if st.button('Update Data'):
-        
-  url_meal_directory = "https://drive.google.com/uc?export=download&id=1yi4afzO20LqU2qdRrtK_g04H7juTzUT8"
-  url_ingredients_directory = "https://drive.google.com/uc?export=download&id=1EOxvPrpOn3NMwmYg5XegtyXdgjq09A9a"
-  filename_meal_directory = wget.download(url_meal_directory)
-  filename_ingredients_directory = wget.download(url_ingredients_directory)
-  df_meal_directory = pd.read_csv(filename_meal_directory)
-  df_ingredient_directory = pd.read_csv(filename_ingredients_directory)
+#if st.button('Update Data'):
+url_meal_directory = "https://drive.google.com/uc?export=download&id=1yi4afzO20LqU2qdRrtK_g04H7juTzUT8"
+url_ingredients_directory = "https://drive.google.com/uc?export=download&id=1EOxvPrpOn3NMwmYg5XegtyXdgjq09A9a"
+filename_meal_directory = wget.download(url_meal_directory)
+filename_ingredients_directory = wget.download(url_ingredients_directory)
+df_meal_directory = pd.read_csv(filename_meal_directory)
+df_ingredient_directory = pd.read_csv(filename_ingredients_directory)
 
 
 # DISPLAY ALL MEALS
