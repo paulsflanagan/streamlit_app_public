5c927806d9e33498a951063dfe1ac2fcae81665f
Paul S Flanagan <paulsflanagan@gmail.com>
2023-08-06 19:34:38 +0100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,12 @@
+import os
+import streamlit as st
+import requests
+import pandas as pd
+
+# define empty df
+#df = pd.DataFrame()
+
+# define logo
+#image = Image.open('Logo.png')
+#st.image(image)
+st.title('AI Assistant')
