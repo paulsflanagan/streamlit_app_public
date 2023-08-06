6e58ef3349742ff5da91eaf61b0e733f6378639a
Paul S Flanagan <paulsflanagan@gmail.com>
2023-08-06 20:37:40 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,30 +23,30 @@ st.write(df_meal_directory)
 
 #st.write(df_ingredient_directory)
 
+if st.button('Generate Meals'):
 
-
-df_menu = pd.DataFrame()
-
-#st.download_button('Download Output', data=data_as_csv, file_name=export_file_name)
-
-while len(df_menu) < 1:
-  df_prospective_meal = df_meal_directory.sample()
-  #if (df_prospective_meal.iloc[0]['dish_name'] not in set (df_exclude_list['dish_name'].values)):
-  if True:
-    df_menu = pd.concat([df_menu, df_prospective_meal])
-
-# Additional Meals
-
-while len(df_menu) < 5:
-  df_prospective_meal = df_meal_directory.sample()
-
-  #if (df_prospective_meal.iloc[0]['dish_name'] not in set (df_exclude_list['dish_name'].values)):
-  if True:
-    if (df_prospective_meal.iloc[0]['dish_name'] not in set(df_menu['dish_name'].values)):
-      if (df_prospective_meal.iloc[0]['style'] not in set(df_menu['style'].values)):
-        if (df_prospective_meal.iloc[0]['main_ingredient'] not in set(df_menu['main_ingredient'].values)):
-          if (df_prospective_meal.iloc[0]['base_ingredient'] not in set(df_menu['base_ingredient'].values)):
-            df_menu = pd.concat([df_menu, df_prospective_meal])
-
-st.write(df_menu)
+  df_menu = pd.DataFrame()
+  
+  #st.download_button('Download Output', data=data_as_csv, file_name=export_file_name)
+  
+  while len(df_menu) < 1:
+    df_prospective_meal = df_meal_directory.sample()
+    #if (df_prospective_meal.iloc[0]['dish_name'] not in set (df_exclude_list['dish_name'].values)):
+    if True:
+      df_menu = pd.concat([df_menu, df_prospective_meal])
+  
+  # Additional Meals
+  
+  while len(df_menu) < 5:
+    df_prospective_meal = df_meal_directory.sample()
+  
+    #if (df_prospective_meal.iloc[0]['dish_name'] not in set (df_exclude_list['dish_name'].values)):
+    if True:
+      if (df_prospective_meal.iloc[0]['dish_name'] not in set(df_menu['dish_name'].values)):
+        if (df_prospective_meal.iloc[0]['style'] not in set(df_menu['style'].values)):
+          if (df_prospective_meal.iloc[0]['main_ingredient'] not in set(df_menu['main_ingredient'].values)):
+            if (df_prospective_meal.iloc[0]['base_ingredient'] not in set(df_menu['base_ingredient'].values)):
+              df_menu = pd.concat([df_menu, df_prospective_meal])
+  
+  st.write(df_menu)
 #st.write(len(df_menu))
