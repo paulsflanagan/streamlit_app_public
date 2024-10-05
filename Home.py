76e4f5eea47cc1c724fdfbb48866fca9cf2bafbd
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-05 15:43:24 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,10 +16,10 @@ userName = st.experimental_user.email
 st.title('Gourmet Guru')
 col1, col2 = st.columns([1, 3])
 st.write("ignore this testing something...")
-col1.image("img/Gochujang Salmon Poke Bowl Small.jpeg")
-col2.write("1. Gochujang Salmon Poke Bowl Small  \n with Sesame Sweet Potato and Smacked Cucumber  \n (Korean)  \n[view recipe](https://drive.google.com/file/d/1bYXra2YLgPNHpicqFlTA7bSHacX8nrYO/view?usp=sharing)")
-col1.image("img/Gochujang Salmon Poke Bowl Small.jpeg")
-col2.write("1. Gochujang Salmon Poke Bowl Small  \n with Sesame Sweet Potato and Smacked Cucumber  \n (Korean)  \n[view recipe](https://drive.google.com/file/d/1bYXra2YLgPNHpicqFlTA7bSHacX8nrYO/view?usp=sharing)")
+col1.image("img/Gochujang Salmon Poke Bowl.jpeg")
+col2.write("1. Gochujang Salmon Poke Bowl  \n with Sesame Sweet Potato and Smacked Cucumber  \n (Korean)  \n[view recipe](https://drive.google.com/file/d/1bYXra2YLgPNHpicqFlTA7bSHacX8nrYO/view?usp=sharing)")
+col1.image("img/Gochujang Salmon Poke Bowl.jpeg")
+col2.write("1. Gochujang Salmon Poke Bowl  \n with Sesame Sweet Potato and Smacked Cucumber  \n (Korean)  \n[view recipe](https://drive.google.com/file/d/1bYXra2YLgPNHpicqFlTA7bSHacX8nrYO/view?usp=sharing)")
 
 with st.sidebar:
         st.title('Settings:')
