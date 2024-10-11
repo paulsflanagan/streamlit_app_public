8c3e3686420f3d8e97735d6e161b8f65dc2b7917
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-11 22:39:45 +0100

    Update Search.py

--- pages/Search.py
+++ pages/Search.py
@@ -60,7 +60,9 @@ if search_string:
     for each in search_string_list:
       if each in dish_name_key_words:
         #st.write(each)
-        df_search_rows = df_meal_directory.loc[df_meal_directory['dish_name'].str.contains(each, case=False) or df_meal_directory['dish_sub_name'].str.contains(each, case=False)]
+        df_search_name_rows = df_meal_directory.loc[df_meal_directory['dish_name'].str.contains(each, case=False)]
+        df_search_sub_name_rows = df_meal_directory.loc[df_meal_directory['dish_sub_name'].str.contains(each, case=False)]
+        df_search_rows = pd.concat([df_search_name_rows, df_search_sub_name_rows])
         search_rows = list(df_search_rows['id'])
         for row in search_rows:
           #st.write(row)
