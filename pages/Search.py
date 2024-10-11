cc7bd6ff414121bcf0dde0ccec910e5b18645f99
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-11 13:26:14 +0100

    Update Search.py

--- pages/Search.py
+++ pages/Search.py
@@ -27,7 +27,11 @@ meal_id = st.chat_input('meal id')
 # Generate Meals
 if meal_id:
   #st.write(df_meal_directory.loc[df_meal_directory['id'] == int(meal_id)])
-  df_display = df_meal_directory.loc[df_meal_directory['id'] == int(meal_id)]
+
+  try:
+    df_display = df_meal_directory.loc[df_meal_directory['id'] == int(meal_id)]
+  except:
+    st.write('')
   for index, row in df_display.iterrows():
     st.image("img/" + str(row['id']) + ". " + row['dish_name'] + ".jpg")
     st.write(str(row['id']) + ". " + row['dish_name'] + "  \n " + row['dish_sub_name'] + "  \n (" + row['style'] + ") - " + "[view]("+ row['link'] +")")
