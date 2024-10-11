8af68a3849d10f4bde5d8c8d14f52765ad9044ac
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-11 22:22:43 +0100

    Update Search.py

--- pages/Search.py
+++ pages/Search.py
@@ -64,9 +64,8 @@ if search_string:
           except:
             result_dictionary[row] = 1
     #here
-    df_results = pd.DataFrame(result_dictionary.items())
+    df_results = pd.DataFrame(result_dictionary.items().sort_values(by=1, ascending=False))
     st.write(df_results)
-    st.write(df_results.sort_values(by=1, ascending=False))
 
   #st.write(result_dictionary)
           #counter = result_dictionary[rows]
