0284d4332a9b62a48c2af4ad674ee598873e56df
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-11 13:16:42 +0100

    Update Search.py

--- pages/Search.py
+++ pages/Search.py
@@ -23,8 +23,7 @@ df_meal_directory = pd.read_csv('meal_directory.csv')
 df_ingredient_directory = pd.read_csv('ingredient_directory.csv')
 
 
-
+meal_id = st.input_text('meal id')
 # Generate Meals
-if st.button('ID Search'):
-  st.write('Hello!')
-  st.write(df_meal_directory.loc[df_meal_directory['id'] == int('3')])
+if st.button('Search'):
+  st.write(df_meal_directory.loc[df_meal_directory['id'] == int(meal_id)])
