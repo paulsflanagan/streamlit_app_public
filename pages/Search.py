d53da29bd4542c62f86845def6c094efbfdef332
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-11 23:45:40 +0100

    Update Search.py

--- pages/Search.py
+++ pages/Search.py
@@ -91,6 +91,7 @@ if search_string:
     #here
     df_results = pd.DataFrame(result_dictionary.items())
     df_results = df_results.sort_values(by=1, ascending=False)
+    df_results = df_results[df_results.1 > 1)
     results = list(df_results[0])
     #st.write(results)
     x = 0
