f08e41cde129b65cd751ce44860a67ec95ae73c6
Paul S Flanagan <paulsflanagan@gmail.com>
2025-02-22 16:56:58 +0000

    Update Sundries_(Sauces_&_Dips).py

--- pages/Sundries_(Sauces_&_Dips).py
+++ pages/Sundries_(Sauces_&_Dips).py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 spb_url = st.secrets["spb_url"]
 spb_key = st.secrets["spb_key"]
 supabase: Client = create_client(spb_url, spb_key)
-userName = st.experimental_user.email
+userName = "Unknown" #st.experimental_user.email
 
 st.title('Gourmet Guru')
 
